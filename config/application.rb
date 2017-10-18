2384d0686ab9d25f11078770dd20ead7ee53524c
Aakanksha <aakanksha0402@gmail.com>
2017-10-18 14:32:02 +0530

    Secret keys

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'json'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,5 +12,16 @@ module Minions
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    creds_file = File.join(Rails.root, 'config', 'secrets.json')
+    creds = JSON.load(File.read(creds_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(creds_file)
+
+    # If file format is yml
+    creds = YAML.load(File.read(creds_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(creds_file)
+
   end
 end
