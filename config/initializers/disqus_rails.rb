51d91728807a925f2cc23063d3264ed3d95f901e
Aakanksha <aakanksha0402@gmail.com>
2018-11-15 11:24:51 +0530

    Implement Disqus and PDF with bootstrap

---
+++ config/initializers/disqus_rails.rb
@@ -0,0 +1,6 @@
+# DisqusRails.setup do |config|
+#   config::SHORT_NAME = "your_short_name"
+#   config::SECRET_KEY = "toOORohQM1s4tmdj9DUSuXnBoCm6hlXfFCacFl4bvFcGEq5eZ0i9l2LE5mhzO2F2" #leave blank if not used
+#   config::PUBLIC_KEY = "your ifuXDSCAfKrVBMlOX58bJZMPM8AGZJLZ8eHJF9gnITPo7JfDOyRdYlq0J3tDbfZw" #leave blank if not used
+#   config::ACCESS_TOKEN = "426582a02a1d4486aa11dcc2322a233c" #you got it, right? ;-)
+# end
